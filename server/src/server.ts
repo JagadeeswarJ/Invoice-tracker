e4d01e4464cdafcf89014d86c79dccbb430361c0
Jagadeeswar_J <115331104+Jagadeeswar072@users.noreply.github.com>
2025-06-20 08:27:58 +0530

    getting client IP

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,6 +9,9 @@ app.get("/", (req: Request, res: Response) => {
     // res.send("Hello, Worldd!");
     console.log(req.ip)
     console.log(req.headers)
+    const forwarded = req.headers['x-forwarded-for'];
+    const ip = typeof forwarded === 'string' ? forwarded.split(',')[0] : req.socket.remoteAddress;
+    console.log("ip from x-forwarded-for:", ip);
 
     // res.send(req.ip);
 
@@ -19,6 +22,6 @@ app.get("/api", (req: Request, res: Response) => {
 });
 
 app.listen(3000, () => {
-    console.log(`Server is running on port 3000`); 
-    
+    console.log(`Server is running on port 3000`);
+
 });
