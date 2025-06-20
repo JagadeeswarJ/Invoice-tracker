c26081d934df7c59840d18105a24285500303254
Jagadeeswar_J <115331104+Jagadeeswar072@users.noreply.github.com>
2025-06-20 22:09:19 +0530

    fix .css module

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#0f323f",
+      },
+    },
   },
   plugins: [],
 };
